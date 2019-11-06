ef446444cf7b490529771b19411207c4d49a22aa
Kristóf <matolcsy.kristof@gmail.com>
2019-11-06 22:28:07 +0100

    merge fix

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 from app import app
 
-<<<<<<< HEAD
 
 if __name__ == '__main__':
         app.run(debug=True)
-=======
-if __name__ == '__main__':
-	print('process starting')
-	app.run(debug=True)
-
->>>>>>> e067cd412fb67043628640a0cd800a5b2e854459
